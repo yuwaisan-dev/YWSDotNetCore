9ea984f02d616598268e3f8ab349570159c76f89
yuwaisan <yuwaisan1078@gmail.com>
2024-09-19 11:03:12 +0630

    feat:EFCoreCRUd

---
+++ YWSDotNetCore.ConsoleApp/AppDbConnect.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YWSDotNetCore.ConsoleApp.Models;
+
+namespace YWSDotNetCore.ConsoleApp
+{
+    public class AppDbConnect:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string connectionString = "Data Source=.;Initial Catalog=YWSDotNetCore;User ID=sa;Password=sasa@123;TrustServerCertificate=True;";
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        public DbSet<BlogDataModel> Blogs { get; set; }
+
+    }
+}
--- YWSDotNetCore.ConsoleApp/Program.cs
+++ YWSDotNetCore.ConsoleApp/Program.cs
@@ -32,13 +32,22 @@ Console.ReadKey();
 //adoDotNetExample.Delete();
 
 //Dapper => CRUD
-DapperExample dapperExample = new DapperExample();
+//DapperExample dapperExample = new DapperExample();
 //dapperExample.Read();
 //dapperExample.Create("dapperTitle", "dapperAuthor", "dapperContent");
-dapperExample.Edit(11);
-dapperExample.Edit(13);
+//dapperExample.Edit(11);
+//dapperExample.Edit(13);
 //dapperExample.Update(13, "testing", "testing", "testing");
 //dapperExample.Delete(11);
+
+
+//EFCore => CRUd
+EFCoreExample eFCoreExample = new EFCoreExample();
+//eFCoreExample.Read();
+//eFCoreExample.Create("efCoreTitle", "efCoreAuthor", "efCoreContent");
+//eFCoreExample.Edit(14);
+//eFCoreExample.Update(14, "test", "test", "test");
+eFCoreExample.Delete(14);
 
 
 Console.ReadKey();

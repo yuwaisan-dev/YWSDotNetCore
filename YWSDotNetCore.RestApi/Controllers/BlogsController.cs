a54e1e9a7cf2d55aae9c94747f844399e79b3976
yuwaisan <yuwaisan1078@gmail.com>
2024-09-24 14:45:53 +0630

    feat:create aspdotnet core  web api

---
+++ YWSDotNetCore.RestApi/Controllers/BlogsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace YWSDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlog()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateBlog()
+        {
+            return Ok();
+        }
+
+        [HttpPatch]
+        public IActionResult PatchBlog()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteBlog()
+        {
+            return Ok();
+        }
+    }
+}

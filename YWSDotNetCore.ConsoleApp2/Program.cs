087750e8b619f2f6147aba92704d1ec99c1b99df
yuwaisan <yuwaisan1078@gmail.com>
2024-09-24 14:43:13 +0630

    feat:create consoleapp2

---
+++ YWSDotNetCore.ConsoleApp2/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using YWSDotNetCore.Database2.Models;
+
+Console.WriteLine("Hello, World!");
+
+AppDbContext db = new AppDbContext();
+var list = db.TblBlogs.ToList();

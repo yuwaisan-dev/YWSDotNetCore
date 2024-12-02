66bd2f6ae1ac4fd877a20a0b9ece58b5ce0efd8f
yuwaisan <yuwaisan1078@gmail.com>
2024-12-02 15:12:35 +0630

    feat:http client(read,post,update)

---
+++ YWSDotNetCore.ConsoleApp3/HttPClientExample.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YWSDotNetCore.ConsoleApp3
+{
+    internal class HttpClientExample
+    {
+        private readonly HttpClient _client = new HttpClient();
+        private readonly string _postEndPoint = "https://jsonplaceholder.typicode.com/posts";
+
+        public HttpClientExample()
+        {
+            _client = new HttpClient();
+        }
+        public async Task Read()
+        {
+            var response = await _client.GetAsync(_postEndPoint);
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(jsonStr);
+            }
+        }
+
+        public async Task Edit(int id)
+        {
+            var response = await _client.GetAsync($"{_postEndPoint}/{id}");
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                Console.WriteLine("No Data Found");
+                return;
+            }
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(jsonStr);
+            }
+        }
+
+        public async Task Create(string title,string body,int userId)
+        {
+            PostModel requestModel = new PostModel()
+            {
+                body = body,
+                title = title,
+                userId = userId
+            };
+           
+            var jsonrequest = JsonConvert.SerializeObject(requestModel);
+            var content = new StringContent(jsonrequest,Encoding.UTF8,Application.Json);
+            var response = await _client.PostAsync(_postEndPoint, content);
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(jsonStr);
+            }
+        }
+
+        public async Task Update(int id,string title, string body, int userId)
+        {
+            PostModel requestModel = new PostModel()
+            {
+                id = id,
+                body = body,
+                title = title,
+                userId = userId
+            };
+
+            var jsonrequest = JsonConvert.SerializeObject(requestModel);
+            var content = new StringContent(jsonrequest, Encoding.UTF8, Application.Json);
+            var response = await _client.PutAsync($"{_postEndPoint}/{id}", content);
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(jsonStr);
+            }
+        }
+    }
+
+    public class PostModel
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+    }
+
+}
---
+++ YWSDotNetCore.ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using YWSDotNetCore.ConsoleApp3;
+
+Console.WriteLine("Hello, World!");
+//get
+//edit
+//post
+//update
+//delete
+
+//endpont
+//resource
+
+
+HttpClientExample httpClientExample = new HttpClientExample();
+await httpClientExample.Read();
+await httpClientExample.Edit(1);
+await httpClientExample.Edit(100);
+await httpClientExample.Create("test title", "test body", 1);
+await httpClientExample.Update(1, "testing", "testing", 1);
